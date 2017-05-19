36b5dbc72e6865e36c286245300de415f85b44cd
rhobernardi <rodrigo.n.bernardi@gmail.com>
2017-05-19 10:44:13 +0000

    adm login added

--- js/sign_in.js
+++ js/sign_in.js
@@ -1,7 +1,9 @@
 
 $(document).ready(function(){
 	var user = "usuario@email";
-	var pass = "12345";
+    var user_pass = "12345";
+    var adm = "adm@email"
+	var adm_pass = "54321";
 	var input_email;
 	var input_pass;
 
@@ -17,7 +19,7 @@ $(document).ready(function(){
 
 				if(input_email === user)
 				{
-					if(input_pass === pass)
+					if(input_pass === user_pass)
 					{
 						console.log("USER E PASS CORRETOS");
 						$('#Sign_in_Form').attr('action', 'client_menu.html');
@@ -30,7 +32,24 @@ $(document).ready(function(){
 						$('#inputPassword').val("");
 					}
 				}
-				else
+                
+                if(input_email === adm)
+				{
+					if(input_pass === adm_pass)
+					{
+						console.log("USER E PASS CORRETOS");
+						$('#Sign_in_Form').attr('action', 'adm_menu.html');
+					}
+					else
+					{
+						$( "#errorPass" ).addClass( "has-error" );
+						$('#Sign_in_Form').attr('action', '#');
+						$('#inputPassword').attr('placeholder', 'Wrong Password');
+						$('#inputPassword').val("");
+					}
+				}
+				
+                else
 				{
 					$( "#errorEmail" ).addClass( "has-error" );
 					$('#Sign_in_Form').attr('action', '#');
